b97119d607683d1fc619b7a7c4eb10c8f313812d
Michael <ndiritumichael5@gmail.com>
2021-11-04 09:20:36 +0300

    Initial Commit

---
+++ ScrumDinger/ScrumDingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumDingerApp.swift
+//  ScrumDinger
+//
+//  Created by mac on 04/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct ScrumDingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
