70445068a5bb793bfd7c654e433402ac6c12b519
Michael <ndiritumichael5@gmail.com>
2021-11-05 14:31:46 +0300

    added image and bottom icons

--- ScrumDinger/ContentView.swift
+++ ScrumDinger/ContentView.swift
@@ -9,19 +9,44 @@ import SwiftUI
 
 struct MeetingView: View {
     var body: some View {
+        let time = "5"
         VStack {
             ProgressView(value: 5 , total: 30)
             HStack{
-                Text("Seconds Elapsed")
-              
+                VStack (alignment:.leading){
+                Label("300",systemImage:"hourglass.bottomhalf.fill")
+                    Text("Seconds Elapsed").font(.caption)
+                }.padding()
                 
-            }
-            Text("Loading")
+                Spacer()
+                VStack(alignment: .leading) {
+                Label("300",systemImage:"hourglass.bottomhalf.fill")
+                    Text("Seconds Remaining").font(.caption)
+                }.padding()
+                
+                
+              
+                      }
+            Circle()
+                .strokeBorder(lineWidth : 24,antialiased: true)
+                .padding()
+            
+            HStack{
+                
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(
+                    action:{}){
+                    Image(systemName: "forward.fill")
+                }
+                    }
+            }.padding()
+                    
             
             
-        }
-        }
+        
     }
+}
 
 
 struct MeetingView_Previews: PreviewProvider {
